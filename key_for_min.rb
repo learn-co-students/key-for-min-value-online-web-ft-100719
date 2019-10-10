7a44f3fb6d26bd319809c143d6a63bc3a4fc8883
Gina Sekhar <ginasekhar@yahoo.com>
2019-10-10 00:55:53 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 require 'pry'
 def key_for_min_value(name_hash)
-  binding.pry
+  
   if name_hash.empty? 
    return NIL
   end
