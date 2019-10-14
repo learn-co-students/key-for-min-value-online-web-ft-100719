dafed2d7d3c656b9276d2c984f9a8335bae383a6
William Pannell <lpann5@gmail.com>
2019-10-14 04:43:55 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,19 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  min=0
+  if name_hash == {}
+       return nil
+  else
+    x=0
+    while x!=3 #name_hash.length
+       if name_hash[x][0] < name_hash[x+1][0]
+       min = name_hash[x][0]
+       else
+       min = name_hash[x+1][0]
+       end
+       x+=1
+    end
+   end
+   return min
 end
