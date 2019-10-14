734fe7fb92c2bea480c7fa21a0418056023bf13d
RokhayaN <rokhayamuslima1@gmail.com>
2019-10-14 19:06:23 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,11 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
-
+coffee_choice ={"expresso": 10
+               "frapucino": 5
+               "creamicino": 4}
+def key_for_min_value(coffee_choice)
+  coffee_choice.each.do |coffee,count|
+    coffee_choice count.min
 end
+  end 
