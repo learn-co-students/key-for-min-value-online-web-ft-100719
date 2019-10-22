df6642bdefd02b4f44f74d6f192683aff337b123
Michael Calhoun <iengineersoftware@gmail.com>
2019-10-22 13:00:14 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,9 +1,13 @@
-# Given a hash with numeric values, return the key for the smallest value
+# Woopsy! Just noticed that the #key and #sort are off limits. Did redo below.
 
 def key_for_min_value(name_hash)
   holder = []
   name_hash.each do |key, value|
-    holder << value
+    if holder == []
+      holder.unshift(value, key)
+    elsif value < holder[0]
+      holder.unshift(value, key)
+    end
   end
-  name_hash.key(holder.sort[0])
+  holder[1]
 end
